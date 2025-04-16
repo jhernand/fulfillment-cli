0989b9b9bb4f35df605afb52fd51f944b59f7f59
Juan Hernández <juan.hernandez@redhat.com>
2025-04-16 15:42:55 +0200

    Add logging utilities (#9)
    This patch adds utilities to configure the `slog` library for the
    project.
    
    The log can be configured with the new `--log-...` command line flags:
    
    ```
    --log-bodies              Include details of HTTP request and response
                              bodies in log messages. Note that currently
                              only the size is written, not the complete body.
    
    --log-field stringArray   Field to add to all log messages. The value can
                              be a percent sign followed by one of the letters
                              that indicate a special value, or a field name
                              followed by an equals sign and the field value.
                              For example '%p' results in a field named 'pid'
                              containing the identifier of the process, and
                              'my-field=my-value' results in adding a field
                              named 'my-field' with value 'my-value'.
    
    --log-fields strings      Comma separated list of fields to add to all
                              log messages. See the '--log-field' option for
                              details of allowed values. Note that this doesn't
                              allow values containing commas, use the
                              '--log-field' option if you need that.
    
    --log-file string         Log file. The value can also be 'stdout'
                              or 'stderr' and then the log will be
                              written to the standard output or error
                              stream of the process. (default "stdout")
    
    --log-headers             Include HTTP headers in log messages.
    
    --log-level string        Log level. Possible values are 'debug',
                              'info', 'warn' and 'error'. (default "error")
    
    --log-redact              Enables or disables redactiong security
                              sensitive data from the log. (default true)
    ```
    
    To avoid mixing with the output the log is by default written to the
    `~/.cache/fulfillment-cli/fulfillment-cli.log` file. That can be changed
    with the `--log-file` command line flag.
    
    The `slog.Logger` object will be available in the context, using the
    `LoggerFromContext` method of the `logging` package. For example:
    
    ```go
    logger := logging.LoggerFromContext(ctx)
    logger.InfoContext(ctx, "Hello!")
    ```
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.9
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.1
+	github.com/onsi/ginkgo/v2 v2.22.2
+	github.com/onsi/gomega v1.36.2
 	github.com/spf13/cobra v1.9.1
 	golang.org/x/oauth2 v0.26.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20250204164813-702378808489
@@ -12,9 +14,21 @@ require (
 )
 
 require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/pprof v0.0.0-20241210010833-40e02aabc2ad // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	golang.org/x/tools v0.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	cloud.google.com/go/compute/metadata v0.5.2 // indirect
+	github.com/DataDog/gostackparse v0.7.0
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/spf13/pflag v1.0.6
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
--- internal/cmd/root_cmd.go
+++ internal/cmd/root_cmd.go
@@ -14,6 +14,10 @@ language governing permissions and limitations under the License.
 package cmd
 
 import (
+	"errors"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 
 	"github.com/innabox/fulfillment-cli/internal/cmd/create"
@@ -23,15 +27,24 @@ import (
 	"github.com/innabox/fulfillment-cli/internal/cmd/getkubeconfig"
 	"github.com/innabox/fulfillment-cli/internal/cmd/login"
 	"github.com/innabox/fulfillment-cli/internal/cmd/logout"
+	"github.com/innabox/fulfillment-cli/internal/logging"
 )
 
 func Root() *cobra.Command {
+	// create the runner and the command:
+	runner := &runnerContext{}
 	result := &cobra.Command{
-		Use:           "fulfillment-cli",
-		Short:         "Command line interface for the fulfillment API",
-		SilenceUsage:  true,
-		SilenceErrors: true,
+		Use:               "fulfillment-cli",
+		Short:             "Command line interface for the fulfillment API",
+		SilenceUsage:      true,
+		SilenceErrors:     true,
+		PersistentPreRunE: runner.persistentPreRun,
 	}
+
+	// Add flags:
+	logging.AddFlags(result.PersistentFlags())
+
+	// Add commands:
 	result.AddCommand(create.Cmd())
 	result.AddCommand(delete.Cmd())
 	result.AddCommand(describe.Cmd())
@@ -39,5 +52,49 @@ func Root() *cobra.Command {
 	result.AddCommand(getkubeconfig.Cmd())
 	result.AddCommand(login.Cmd())
 	result.AddCommand(logout.Cmd())
+
 	return result
+}
+
+type runnerContext struct {
+}
+
+func (c *runnerContext) persistentPreRun(cmd *cobra.Command, args []string) error {
+	// In order to avoid mixing log messages with output we configure the log to go by default to a file in the user
+	// cache directory.
+	//
+	// The path of the cache directory and of the log file are calculated from the name from the name of the binary.
+	// For example, if the name of the binary is `fulfillment-cli` then the cache directory will be
+	// `~/.cache/fulfillment-cli` and the log file will be `~/.cache/fufillment-cli/fulfillment-cli.log`.
+	baseName := filepath.Base(os.Args[0])
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return err
+	}
+	cacheDir := filepath.Join(userCacheDir, baseName)
+	err = os.MkdirAll(cacheDir, 0700)
+	if errors.Is(err, os.ErrExist) {
+		err = nil
+	}
+	if err != nil {
+		return err
+	}
+	logFile := filepath.Join(cacheDir, baseName+".log")
+
+	// By the default the logger is configured to write to the log file, and only errors. This Will be overriden by
+	// the command line flags.
+	logger, err := logging.NewLogger().
+		SetFile(logFile).
+		SetFlags(cmd.Flags()).
+		Build()
+	if err != nil {
+		return err
+	}
+
+	// Replace the default context with one that contains the logger:
+	ctx := cmd.Context()
+	ctx = logging.LoggerIntoContext(ctx, logger)
+	cmd.SetContext(ctx)
+
+	return nil
 }
